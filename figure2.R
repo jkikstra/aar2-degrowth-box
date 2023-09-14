2e5680141d34eca54b3f3af6a947f4b36caf3581
Jarmo Kikstra <46967503+jkikstra@users.noreply.github.com>
2023-09-14 09:45:00 +0200

    Add decoupling panels (v1 - note; labels need fixing)

--- figure2.R
+++ figure2.R
@@ -65,7 +65,7 @@ p.scaleup.levels.40k <- ggplot(li.raw.notnorm %>%
                              filter(
                                year %in% upsc.yrs,
                                `Annual consumption per capita (at utility peak)`=="40k"
-                             ) %>% mutate(`Economic trend` = "Stopping GDP growth") %>%
+                             ) %>% mutate(`Economic trend` = "Slowing GDP growth") %>%
                              bind_rows(baselines %>% rename(value=baseline) %>% mutate(`Economic trend` = "Continuing GDP growth")) %>%
                              filter(
                                `GHG budget`==budget.choice.highlight
@@ -96,14 +96,15 @@ p.scaleup.levels.40k <- ggplot(li.raw.notnorm %>%
 
 
 
-  scale_colour_manual(breaks = c("Continuing GDP growth", "Stopping GDP growth"), values = c("grey", "dodgerblue")) +
+  scale_colour_manual(breaks = c("Continuing GDP growth", "Slowing GDP growth"), values = c("grey", "dodgerblue")) +
   # scale_x_continuous(breaks = upsc.yrs) +
   scale_x_continuous(breaks = seq(2020,2100,20)) +
   scale_y_continuous(limits = c(0,9), expand = c(0,0)) +
   theme_classic() +
   theme_hc() +
   ylab("EJ/yr") + xlab(NULL) +
-  labs(title = "Primary energy supply technology change under different growth pathways\nfor an ambitious emissions reduction scenario.",
+  labs(title = "Primary energy supply technology change under different growth pathways",
+       subtitle = "For a the same emissions reduction target (4GtCO2 GHG until net zero GHG).",
        caption = "Based on the Australian energy system as in Kikstra et al. 2023.") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
         legend.position = "none")
@@ -112,7 +113,159 @@ p.scaleup.levels.40k
 
 
 
+
 # SAVE ====
 save_ggplot(p = p.scaleup.levels.40k,
             f = here("figures", "kikstra-renewables-upscaling"),
             h=120, w=200)
+
+
+# add a decoupling plot ===
+decl.years <- seq(2020,2100,10)
+
+li.emissions.gdp <- vroom(here("data-raw", "kikstra2023", "for-reviewers_towardsdegrowth-v1", "data", "li.csv")) %>%
+  add_degrowth_level() %>%
+  add_scenario_set_type() %>%
+  filter(
+    variable %in% c("GDP (PPP)","GHG emissions", "Final Energy")
+  ) %>%
+  filter(
+    `Climate policy` == "GHG budget",
+    `GHG budget` == "4Gt",
+
+  )
+
+data.ghg.decoupling.40k.A <- li.emissions.gdp %>%
+  filter(
+    year %in% decl.years,
+    `Annual consumption per capita (at utility peak)`=="40k"
+  ) %>% mutate(`Economic trend` = "Slowing GDP growth") %>%
+  bind_rows(li.emissions.gdp %>%
+              filter(
+                year %in% decl.years,
+                `Annual consumption per capita (at utility peak)`=="baseline"
+              ) %>%
+              mutate(`Economic trend` = "Continuing GDP growth"))
+
+p.ghg.decoupling.40k.A <- ggplot(data.ghg.decoupling.40k.A %>%
+                                   normalise_iamc_long(starting.year = 2020),
+                               aes(colour = `Economic trend`)) +
+  geom_hline(yintercept = 0, colour="darkgrey") +
+
+
+  geom_line(
+    data = . %>% filter(variable%in%c("GHG emissions", "Final Energy")),
+    aes(x=year,
+        y=value,
+        linetype=variable),
+    linewidth=2
+  ) +
+  geom_textpath(
+    data = . %>% filter(variable=="GDP (PPP)"),
+    aes(x=year,
+        y=value,
+        linetype=variable,
+        label=`Economic trend`),
+    hjust=.75,
+    linewidth=2
+  ) +
+
+  scale_colour_manual(breaks = c("Continuing GDP growth", "Slowing GDP growth"), values = c("grey", "dodgerblue")) +
+  # scale_x_continuous(breaks = upsc.yrs) +
+  scale_x_continuous(breaks = decl.years) +
+  scale_y_continuous(#limits = c(0,2.5), expand = c(0,0),
+                     labels = scales::percent) +
+  theme_classic() +
+  theme_hc() +
+  ylab(NULL) + xlab(NULL) +
+  labs(title = "Change in economic activity and emissions",
+       subtitle = "Relative to 2020",
+       # caption = "Based on the Australian energy system as in Kikstra et al. 2023."
+       ) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  guides(linetype=guide_legend("Indicator"),
+         color="none")
+
+p.ghg.decoupling.40k.A
+
+
+
+data.ghg.decoupling.40k.B <- data.ghg.decoupling.40k.A %>% select(-unit) %>%
+  pivot_wider(names_from = variable, values_from = value) %>%
+  mutate(value = `GHG emissions`/`GDP (PPP)` / 1e3,
+         variable = "GHG intensity of economic activity",
+         unit = "tCO2-eq/$")
+data.fe.decoupling.40k.B <- data.ghg.decoupling.40k.A %>% select(-unit) %>%
+  pivot_wider(names_from = variable, values_from = value) %>%
+  mutate(value = `Final Energy`/`GDP (PPP)` * 1e3,
+         variable = "Final energy intensity of economic activity",
+         unit = "MJ/$")
+
+
+p.ghg.decoupling.40k.B <- ggplot(data.ghg.decoupling.40k.B,
+                                 aes(colour = `Economic trend`)) +
+  geom_hline(yintercept = 0, colour="darkgrey") +
+
+
+  geom_line(
+    aes(x=year,
+        y=value,
+        linetype=variable),
+    linewidth=2
+  ) +
+
+  scale_colour_manual(breaks = c("Continuing GDP growth", "Slowing GDP growth"), values = c("grey", "dodgerblue")) +
+  # scale_x_continuous(breaks = upsc.yrs) +
+  scale_x_continuous(breaks = decl.years) +
+  # scale_y_continuous(limits = c(0,2.5), expand = c(0,0), labels = ) +
+  theme_classic() +
+  theme_hc() +
+  ylab("tCO2/$") + xlab(NULL) +
+  labs(title = "Emissions intensity",
+       subtitle = "GHG emissions per $",
+       # caption = "Based on the Australian energy system as in Kikstra et al. 2023."
+  ) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  guides(linetype="none",#guide_legend("Indicator"),
+         color="none")
+
+p.ghg.decoupling.40k.B
+
+p.fe.decoupling.40k.B <- ggplot(data.fe.decoupling.40k.B,
+                                 aes(colour = `Economic trend`)) +
+  geom_hline(yintercept = 0, colour="darkgrey") +
+
+
+  geom_line(
+    aes(x=year,
+        y=value,
+        linetype=variable),
+    linewidth=2
+  ) +
+
+  scale_colour_manual(breaks = c("Continuing GDP growth", "Slowing GDP growth"), values = c("grey", "dodgerblue")) +
+  # scale_x_continuous(breaks = upsc.yrs) +
+  scale_x_continuous(breaks = decl.years) +
+  # scale_y_continuous(limits = c(0,2.5), expand = c(0,0), labels = ) +
+  theme_classic() +
+  theme_hc() +
+  ylab("MJ/$") + xlab(NULL) +
+  labs(title = "Final energy intensity",
+       subtitle = "Energy use per $",
+       # caption = "Based on the Australian energy system as in Kikstra et al. 2023."
+  ) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  guides(linetype="none",#guide_legend("Indicator"),
+         color="none")
+
+p.fe.decoupling.40k.B
+
+p.decoupling <- ((p.scaleup.levels.40k) / (p.ghg.decoupling.40k.A | p.ghg.decoupling.40k.B | p.fe.decoupling.40k.B)) +
+  plot_layout() #+
+  # plot_annotation(tag_level = "A")
+
+p.decoupling
+
+save_ggplot(p = p.decoupling,
+            f = here("figures", "kikstra-renewables-upscaling-with-decoupling"),
+            h=200, w=280)
